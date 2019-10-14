0d77238a43bad9af3b6ce1c69c4086c1ac2fcfd2
carlos david camacho panche <carlos0camacho@gmail.com>
2019-10-14 11:22:40 -0500

    Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
+import { Routes, RouterModule, Router } from '@angular/router';
+import { AboutComponent } from './Components/about/about.component';
+import { HeroesComponent} from './Components/heroes/heroes.component';
+import { BodyComponent } from './Components/body/body.component';
 
-const routes: Routes = [];
+const APP_ROUTES: Routes = [
+  {path: 'home', component: BodyComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'heroes', component: HeroesComponent},
+  {path:  '**', pathMatch: 'full', redirectTo: ''}
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash : true});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { APP_ROUTING } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Servicios
 
+// Componentes
 import {HeaderComponent} from './Components/header/header.component';
 import {BodyComponent} from './Components/body/body.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { HeroesComponent } from './Components/heroes/heroes.component';
+import { AboutComponent } from './Components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    HeroesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
